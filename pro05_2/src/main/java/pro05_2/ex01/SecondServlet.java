d422fd4900a4992ee814ebd2779f24e728bc8957
woori <woori@DESKTOP-3T7OJU9>
2023-05-15 15:14:35 +0900

    아무거나

---
+++ pro05_2/src/main/java/pro05_2/ex01/SecondServlet.java
@@ -0,0 +1,43 @@
+package pro05_2.ex01;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//@어노테이션(현재 위치에 기능을 주입)
+//@WebServlet("/접속명")-사용자가 임의변경
+//어노테이션을 사용하면 web.xml 수정할 필요가 없다.
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("SecondServlet에 요청시 화면");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
